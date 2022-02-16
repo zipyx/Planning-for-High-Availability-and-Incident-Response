89a8e4273dbd846d57dae558f1075d53d09079d7
Setephano <setephano@gmail.com>
2022-02-16 23:52:41 +1300

    Adding local changes into repo

--- lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
+++ lesson-4-deploying-terraform-assets/exercises/starter/zone1/modules/ec2/ec2.tf
@@ -6,10 +6,37 @@ resource "aws_instance" "ubuntu" {
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
   tags = {
-    Name = ""
+    Name = "Ubuntu-Web"
   }
 }
 
 resource "aws_security_group" "ec2_sg" {
- 
+  name        = "ec2_sg"
+  vpc_id      = var.vpc_id
+
+  ingress {    
+    description = "web port"
+    from_port   = 80    
+    to_port     = 80
+    protocol    = "tcp"    
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "ssh port"
+    from_port   = 22    
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ec2_sg"
+  }
 }
